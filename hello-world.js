236f6012a4a01f6aa8e9e4989a2f42732bfe1bc0
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-05-23 22:04:31 -0300

    Hello World

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,8 @@
 console.log("Hello World")
+
+let js = "Amazing";
+
+if(js === "amazing") {console.log("JavaScript is FUN!!!")} else {console.log("Javacript is not FUN!!!")};
+
+js = "boring";
+console.log(js)
