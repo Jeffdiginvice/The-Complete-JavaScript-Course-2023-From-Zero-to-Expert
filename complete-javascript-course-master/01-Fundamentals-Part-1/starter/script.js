334e1d1fd04e700cad6d6678333b75f001a24007
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-08-15 17:04:35 -0300

    Data Types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,6 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN");
-console.log(40 + 8 + 23 - 10);
+// if (js === "amazing") alert("Javascript is FUN");
+// console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
@@ -11,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+//variable name conventions
 let jonas_matilda = 'JM';
 let $function = 27;
 
@@ -25,3 +26,24 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+
+ 
+ let javascriptIsFun = true;
+ console.log(javascriptIsFun);
+
+ console.log(typeof true);
+ console.log(typeof javascriptIsFun);
+ console.log(typeof 23);
+ console.log(typeof 'true');
+
+ javascriptIsFun = 'yes';
+ console.log(javascriptIsFun)
+
+ let year; 
+ console.log(year);
+ console.log(typeof year);
+
+ year = 1991;
+ console.log(typeof year);
+
+ console.log(typeof null);
