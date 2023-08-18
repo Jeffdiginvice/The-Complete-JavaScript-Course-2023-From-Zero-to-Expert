bdd61c2470d25a3dbcb2233adf2c1a2977707680
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-08-18 18:23:00 -0300

    Basic Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -49,10 +49,40 @@ console.log(myFirstJob);
  console.log(typeof null)
  */
 
- let age = 30;
- age = 31;
+//  let age = 30;
+//  age = 31;
 
- const birthYear = 1991;
- birthYear = 1990;
+//  const birthYear = 1991;
+//  birthYear = 1990;
 
-  
+const now = 2037;  
+const ageJonas = now - 1991;
+  const ageSarah = now - 2020;
+  console.log(ageSarah, ageJonas)
+  console.log(ageJonas);
+
+  console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+  // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+  const firstName = 'Jonas';
+  const lastName = 'Schedtman';
+  console.log(firstName + ' ' + lastName);
+
+  // assignment operators
+  let x = 10 + 5; //15
+  x += 10; // x = x + 10
+  x *= 4; // x =  x * 4 = 100
+  x++; // x = x + 1
+  x--; // x = x - 1
+  console.log(x);
+
+  // Comparison operators 
+
+  // >, <, >=, <=
+  console.log(ageJonas > ageSarah);
+  console.log(ageJonas < ageSarah);
+  console.log(ageSarah >= 18);
+
+  const isFullAge = ageSarah >= 18;
+
+  console.log(now - 1991 > now - 2018);
