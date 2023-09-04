5665a76b040b86220c412c5cf141d0cbcd6c26bb
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-09-04 17:04:07 -0300

    Coding Challenge

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,52 @@
-// 
+// // 
 
-const hasDriversLicense = true; // A
-const hasGoodVision = true; // B
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
 
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasDriversLicense);
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
 
-const shouldDrive = hasDriversLicense && hasGoodVision;
+// const shouldDrive = hasDriversLicense && hasGoodVision;
 
-// if(shouldDrive){
+// // if(shouldDrive){
+// //   console.log('Sarah is able to drive!');
+// // } else {
+// //   console.log('Someone else should drive...');
+// // }
+
+// const isTired = true; // C
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired){
 //   console.log('Sarah is able to drive!');
 // } else {
 //   console.log('Someone else should drive...');
 // }
 
-const isTired = true; // C
-console.log(hasDriversLicense && hasGoodVision && isTired);
+//  const scoreDolphins = (96 + 108 +89) / 3;
+//  const scoreKoalas = (88 + 91 + 110) / 3;
+//  console.log(scoreDolphins, scoreKoalas);
 
-if(hasDriversLicense && hasGoodVision && !isTired){
-  console.log('Sarah is able to drive!');
-} else {
-  console.log('Someone else should drive...');
-}
+//  if(scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+//  } else if(scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+//  } else if(scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+//  }
+
+ //BONUS 1
+ const scoreDolphins = (97 + 112 + 80) / 3;
+ const scoreKoalas = (109 + 95 + 50) / 3;
+ console.log(scoreDolphins, scoreKoalas);
+
+ if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+ } else if(scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy 🏆');
+ } else if(scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreDolphins >= 100) {
+  console.log('Both win the trophy!');
+ } else {
+  console.log('No one wins the trophy')
+ }
