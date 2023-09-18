7fcaac9f4e16316c1ff38e86619ec42e23f16e67
Jefferson Castro <jeffersondiginvice7@gmail.com>
2023-09-18 20:16:12 -0300

    Reviewing Functions

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
+ 
+// const interface = 'audio'
+// const private = 534;
+// const if = 23;
